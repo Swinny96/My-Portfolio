74f0c64dcf59b3e6396c62dcbb0c35962b39cd21
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-03-09 15:27:33 +0000

    Percentages Changed in Skills

--- src/components/sections/SkillSection.js
+++ src/components/sections/SkillSection.js
@@ -3,46 +3,19 @@ import styled from "styled-components"
 import Skill from "../modules/Skill"
 
 const SkillSection = ({ siteTitle }) => (
-    <Section>
-        <Title>Skills</Title>
-            <Skill
-            percentage="100"
-            skill="Adobe CS"
-            />
-            <Skill
-            percentage="100"
-            skill="HTML & CSS"
-            />
-            <Skill
-            percentage="75"
-            skill="PHP"
-            />
-            <Skill
-            percentage="75"
-            skill="Sketch"
-            />
-            <Skill
-            percentage="75"
-            skill="Figma"
-            />
-            <Skill
-            percentage="50"
-            skill="Javascript"
-            />
-            <Skill
-            percentage="50"
-            skill="React"
-            />
-            <Skill
-            percentage="50"
-            skill="Alexa Skill"
-            />
-            <Skill
-            percentage="75"
-            skill="Wordpress"
-            />
-    </Section>
-  )
+  <Section>
+    <Title>Skills</Title>
+    <Skill percentage="100" skill="Adobe CS" />
+    <Skill percentage="100" skill="HTML & CSS" />
+    <Skill percentage="75" skill="PHP" />
+    <Skill percentage="75" skill="Sketch" />
+    <Skill percentage="75" skill="Figma" />{" "}
+    <Skill percentage="75" skill="React" />
+    <Skill percentage="50" skill="Javascript" />
+    <Skill percentage="50" skill="Alexa Skill" />
+    <Skill percentage="50" skill="Wordpress" />
+  </Section>
+)
 
 export default SkillSection
 
@@ -50,5 +23,4 @@ const Section = styled.section`
   border-bottom: 1px solid #eee;
   padding: 10px 0px;
 `
-const Title = styled.h2`
-`
+const Title = styled.h2``
